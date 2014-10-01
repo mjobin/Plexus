a464d93116a9e9487f4bc250792371a0f9adb7d2
matt <matt@Hearth.local>
2014-10-01 16:48:13 -0700

    Initial Commit

---
+++ Plexus/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Plexus
+//
+//  Created by matt on 10/1/14.
+//  Copyright (c) 2014 Santa Clara University. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
