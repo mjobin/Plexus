4f747e80db0666265810931b2fbe3acceffe8bb8
matt <matt@Hearth.local>
2014-10-01 17:14:16 -0700

    initial import

---
+++ Plexus/PlexusMainSplitViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PlexusMainSplitViewController.swift
+//  Plexus
+//
+//  Created by matt on 10/1/14.
+//  Copyright (c) 2014 Santa Clara University. All rights reserved.
+//
+
+import Cocoa
+
+class PlexusMainSplitViewController: NSSplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ Plexus/PlexusMainWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  PlexusMainWindowController.swift
+//  Plexus
+//
+//  Created by matt on 10/1/14.
+//  Copyright (c) 2014 Santa Clara University. All rights reserved.
+//
+
+import Cocoa
+
+class PlexusMainWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
