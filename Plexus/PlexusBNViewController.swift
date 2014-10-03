919dee0b8ce85706e6f7f808cd8c29be4ad40114
matt <matt@Hearth.local>
2014-10-03 15:21:48 -0700

    initial import

---
+++ Plexus/PlexusBNViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PlexusBNViewController.swift
+//  Plexus
+//
+//  Created by matt on 10/2/14.
+//  Copyright (c) 2014 Santa Clara University. All rights reserved.
+//
+
+import Cocoa
+import SpriteKit
+
+class PlexusBNViewController: NSViewController {
+    
+    @IBOutlet weak var skView: SKView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+    }
+    
+}
