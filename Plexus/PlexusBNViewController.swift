a6b229d7bad650af622aed20dd46d29bbad9998f
matt <matt@Hearth.local>
2014-10-05 12:27:27 -0700

    presents nodes skview

--- Plexus/PlexusBNViewController.swift
+++ Plexus/PlexusBNViewController.swift
@@ -15,7 +15,19 @@ class PlexusBNViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        var scene: PlexusBNScene = PlexusBNScene(size: self.skView.bounds.size)
         
+         scene.scaleMode = SKSceneScaleMode.Fill
+        
+        
+       // var skt : SKTransition = SKTransition.flipHorizontalWithDuration(2)
+       // self.skView!.presentScene(scene, transition: skt)
+
+        
+        self.skView!.presentScene(scene)
+        
+        self.skView!.showsFPS = true
+        self.skView!.showsNodeCount = true
         
 
     }
