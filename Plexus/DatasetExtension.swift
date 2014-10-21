32d7e836009ed0974dd3cdb44182a33fb1b08962
matt <matt@Hearth.local>
2014-10-21 14:58:54 -0700

    initial import

---
+++ Plexus/DatasetExtension.swift
@@ -0,0 +1,18 @@
+//
+//  DatasetExtension.swift
+//  Plexus
+//
+//  Created by matt on 10/21/14.
+//  Copyright (c) 2014 Santa Clara University. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension Dataset {
+    func addEntryObject(value:Entry) {
+        var items = self.mutableSetValueForKey("entry");
+        items.addObject(value)
+    }
+}
