e0937c5c2e6b6c63446173bbfc5ee6d2442e15f6
matt <matt@Hearth.local>
2014-10-23 16:13:14 -0700

    basic set up of image view done, but binding is not working

--- Plexus/PlexusImageViewController.swift
+++ Plexus/PlexusImageViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import Cocoa
+import Quartz
+import AppKit
 
 class PlexusImageViewController: NSViewController {
  
     var moc : NSManagedObjectContext!
     
     dynamic var entryTreeController : NSTreeController!
+    @IBOutlet var imageController : NSArrayController!
+    @IBOutlet var imageBrowser: IKImageBrowserView!
     
     
     required init?(coder aDecoder: NSCoder)
@@ -29,7 +33,51 @@ class PlexusImageViewController: NSViewController {
         super.viewDidLoad()
         // Do view setup here.
         
+        imageBrowser.setDelegate(self)
+        
 
     }
+    
+    @IBAction func addImages(sender: AnyObject){
+
+        
+        var errorPtr : NSErrorPointer = nil
+        
+        let op:NSOpenPanel = NSOpenPanel()
+        op.allowsMultipleSelection = false
+        op.canChooseDirectories = false
+        op.canChooseFiles = true
+       // op.allowedFileTypes = ["csv"]
+        op.runModal()
+        
+        var inFile = op.URL
+        
+        op.orderOut(self)
+        op.close()
+        
+        
+        if (inFile != nil){ // operate on iput file
+            
+            var newImage : Image = Image(entity: NSEntityDescription.entityForName("Image", inManagedObjectContext: moc)!, insertIntoManagedObjectContext: moc)
+            newImage.setValue(inFile!.lastPathComponent, forKey: "imageName")
+            
+            var newRep : NSData = NSData(contentsOfURL: inFile!)!
+            newImage.setValue(newRep, forKey: "imageRepresentation")
+        
+            
+            imageController.addObject(newImage)
+            
+            moc.save(errorPtr)
+            
+        }
+    }
+    /*
+    override func numberOfItemsInImageBrowser(view: IKImageBrowserView) -> Int {
+        //return imageController.
+    }
+    override func imageBrowser(view: IKImageBrowserView, itemAtIndex index: Int) -> AnyObject {
+        return imageContro
+    }
+    */
     
 }
