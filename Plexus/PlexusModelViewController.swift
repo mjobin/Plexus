1a522745310a1663c287cda525409fd48e553495
matt <matt@Hearth.local>
2014-11-05 16:12:16 -0800

    initial import

---
+++ Plexus/PlexusModelViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PlexusModelViewController.swift
+//  Plexus
+//
+//  Created by matt on 10/15/14.
+//  Copyright (c) 2014 Santa Clara University. All rights reserved.
+//
+
+import Cocoa
+
+class PlexusModelViewController: NSViewController {
+
+    var moc : NSManagedObjectContext!
+    dynamic var datasetController : NSArrayController!
+    @IBOutlet dynamic var modelTreeController : NSTreeController!
+    
+    
+    required init?(coder aDecoder: NSCoder)
+    {
+        
+        let appDelegate : AppDelegate = NSApplication.sharedApplication().delegate as AppDelegate
+        moc = appDelegate.managedObjectContext
+        
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
