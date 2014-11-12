de014378b69550d109d073ba131672f5b6b6f7de
matt <matt@Hearth.local>
2014-11-12 07:44:01 -0800

    subclass to add link to BNNode

---
+++ Plexus/PlexusBNNode.swift
@@ -0,0 +1,15 @@
+//
+//  PlexusBNNode.swift
+//  Plexus
+//
+//  Created by matt on 11/10/14.
+//  Copyright (c) 2014 Santa Clara University. All rights reserved.
+//
+
+import Cocoa
+import SpriteKit
+
+class PlexusBNNode: SKShapeNode {
+    var node : BNNode!
+
+}
