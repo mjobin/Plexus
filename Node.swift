b870b7c077c54141ef3b03a28ee31c230920837c
matt <matt@Hearth.local>
2014-11-12 07:43:27 -0800

    initial import

---
+++ Node.swift
@@ -0,0 +1,16 @@
+//
+//  Plexus.swift
+//  Plexus
+//
+//  Created by matt on 11/10/14.
+//  Copyright (c) 2014 Santa Clara University. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Node: NSManagedObject {
+
+    @NSManaged var name: String
+
+}
---
+++ Plexus/BNNode.swift
@@ -0,0 +1,16 @@
+//
+//  BNNode.swift
+//  Plexus
+//
+//  Created by matt on 11/10/14.
+//  Copyright (c) 2014 Santa Clara University. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class BNNode: Node {
+
+    @NSManaged var model: Model
+
+}
