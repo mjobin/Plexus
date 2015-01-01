3a1be7d25bfa277233f487e9b7c21b509690b8ff
matt <matt@Hearth.local>
2015-01-01 14:13:35 -0800

    added modelTableViewController

--- Plexus/PlexusModelTabViewController.swift
+++ Plexus/PlexusModelTabViewController.swift
@@ -11,21 +11,28 @@ import Cocoa
 class PlexusModelTabViewController: NSTabViewController {
     
     dynamic var modelTreeController : NSTreeController!
+
     
     var bnViewController : PlexusBNViewController? = nil
+    var modelTableViewController : PlexusModelTableViewController? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
         
         bnViewController = childViewControllers[0] as? PlexusBNViewController
+        modelTableViewController = childViewControllers[1] as? PlexusModelTableViewController
+        
+        
     }
     
     override func viewDidAppear() {
         super.viewDidAppear()
         
         
+        
         bnViewController!.modelTreeController = self.modelTreeController
+        modelTableViewController!.modelTreeController = self.modelTreeController
 
     }
     
