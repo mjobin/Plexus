09c7bc0dcf553eca78987ae096aa3294cf1a221d
matt <matt@Hearth.local>
2015-01-01 14:14:44 -0800

    initil import

---
+++ Plexus/PlexusModelTableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  PlexusModelTableViewController.swift
+//  Plexus
+//
+//  Created by matt on 12/26/2014.
+//  Copyright (c) 2014 Santa Clara University. All rights reserved.
+//
+
+import Cocoa
+
+class PlexusModelTableViewController: NSViewController {
+    
+    var moc : NSManagedObjectContext!
+    
+    dynamic var modelTreeController : NSTreeController!
+
+    
+    required init?(coder aDecoder: NSCoder)
+    {
+        
+        let appDelegate : AppDelegate = NSApplication.sharedApplication().delegate as AppDelegate
+        moc = appDelegate.managedObjectContext
+        
+        super.init(coder: aDecoder)
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+        
+        
+    }
+    
+    override func viewDidAppear() {
+        super.viewDidAppear()
+        
+
+    }
+    
+}
