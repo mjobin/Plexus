fc7c718c0c268422b2ef27961b8119d3c4b03a5e
matt <matt@Hearth.local>
2015-01-06 08:09:34 -0800

    simple but visible graph... can work from here to add data needed

--- Plexus/PlexusBNSingleNodeViewController.swift
+++ Plexus/PlexusBNSingleNodeViewController.swift
@@ -30,9 +30,9 @@ class PlexusBNSingleNodeViewController: NSViewController, CPTScatterPlotDataSour
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        visView.blendingMode = NSVisualEffectBlendingMode.BehindWindow
-        visView.material = NSVisualEffectMaterial.Dark
-        visView.state = NSVisualEffectState.Active
+     //   visView.blendingMode = NSVisualEffectBlendingMode.BehindWindow
+      //  visView.material = NSVisualEffectMaterial.Dark
+      //  visView.state = NSVisualEffectState.Active
         
         var graph = CPTXYGraph(frame:self.graphView.bounds);
         self.graphView.hostedGraph = graph
@@ -42,22 +42,57 @@ class PlexusBNSingleNodeViewController: NSViewController, CPTScatterPlotDataSour
         
         var plotSpace : CPTXYPlotSpace = graph.defaultPlotSpace as CPTXYPlotSpace
         plotSpace.allowsUserInteraction = false
+        var xRange = plotSpace.xRange.mutableCopy() as CPTMutablePlotRange
+        var yRange = plotSpace.yRange.mutableCopy() as CPTMutablePlotRange
+        xRange.setLengthFloat(10)
+        yRange.setLengthFloat(10)
+        plotSpace.xRange = xRange
+        plotSpace.yRange = yRange
+        
         
         var priorPlot = CPTScatterPlot(frame:graph.bounds)
+        
+       // priorPlot.interpolation = CPTScatterPlotInterpolationCurved
         
         priorPlot.dataSource = self
         priorPlot.delegate = self
         
         //FIXME dummy data
         self.dataForChart = [0.6, 0.4, 0.2, 0.08]
+
         
         graph.addPlot(priorPlot)
+
+        
+        //println(graphView.bounds)
+        /*
+        var pieChart = CPTPieChart(frame:graphView.bounds)
+        pieChart.pieRadius = 80.0;
+        
+        pieChart.dataSource = self;
+        pieChart.delegate = self;
+        
+        self.dataForChart = [40, 30, 20, 10];
+        
+        graph.addPlot(pieChart);
+*/
+
         
 
     }
     
     func numberOfRecordsForPlot(plot: CPTPlot!) -> UInt {
-        println("self.dataForChart.count: \(UInt(self.dataForChart.count))");
+        return 4
+    }
+    
+    func numberForPlot(plot: CPTPlot!, field fieldEnum: UInt, recordIndex idx: UInt) -> NSNumber! {
+        return idx+1
+    }
+    
+    /*
+    
+    func numberOfRecordsForPlot(plot: CPTPlot!) -> UInt {
+      //  println("self.dataForChart.count: \(UInt(self.dataForChart.count))");
         return UInt(self.dataForChart.count)
     
     }
@@ -66,5 +101,6 @@ class PlexusBNSingleNodeViewController: NSViewController, CPTScatterPlotDataSour
         println("numberForPlot")
         return self.dataForChart[index] as NSNumber
     }
+*/
     
 }
