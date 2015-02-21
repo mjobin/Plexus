9de0b63ae4334d88cd6b289053847b10ed34c393
matt <matt@Hearth.local>
2015-02-21 10:30:51 -0800

    Importing.

---
+++ Plexus/Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  Bridging-Header.h
+//  Plexus
+//
+//  Created by matt on 1/1/2015.
+//  Copyright (c) 2015 Santa Clara University. All rights reserved.
+//
+
+#ifndef Plexus_Bridging_Header_h
+#define Plexus_Bridging_Header_h
+
+#import <CorePlot/CorePlot.h>
+#import "CPTMutablePlotRange+SwiftCompat.h"
+#import "PlexusCalculationOperation.h"
+//#import "bnkernel.cl.h"
+//#import "bn_kernel.cl"
+#endif
