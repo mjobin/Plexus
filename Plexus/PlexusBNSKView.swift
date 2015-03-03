e8e6efa9329876fd1b7f01fe52fe75cd68ee2814
matt <matt@Hearth.local>
2015-03-03 14:59:11 -0800

    removed printlns

--- Plexus/PlexusBNSKView.swift
+++ Plexus/PlexusBNSKView.swift
@@ -19,7 +19,6 @@ class PlexusBNSKView: SKView, NSDraggingDestination {
     {
         
         super.init(coder: aDecoder)
-        println("bnskview init")
         var registeredTypes:[String] = [kUTTypeURL]
         self.registerForDraggedTypes(registeredTypes)
         
@@ -36,14 +35,9 @@ class PlexusBNSKView: SKView, NSDraggingDestination {
     }
     
     override func draggingEntered(sender: NSDraggingInfo) -> NSDragOperation {
-        println("dragging on bnskview")
         
         return NSDragOperation.Copy
         
-    }
-    
-    override func draggingExited(sender: NSDraggingInfo?) {
-        println("dragging ENDED on bnskview")
     }
     
     override func prepareForDragOperation(sender: NSDraggingInfo) -> Bool {
@@ -54,67 +48,45 @@ class PlexusBNSKView: SKView, NSDraggingDestination {
         let pboard : NSPasteboard = sender.draggingPasteboard()
         let types : NSArray = pboard.types!
         
-        println(types)
         
-
         
         let data : NSData = pboard.dataForType(kUTTypeURL)! as NSData
         let draggedArray : NSArray = NSKeyedUnarchiver.unarchiveObjectWithData(data) as NSArray
         
-        // Loop through DraggedArray
-        for object : AnyObject in draggedArray{
-            
-            println(object)
-            self.addNode(object as NSURL)
-            //let mo : NSURL = object as NSURL
-            
-            
-            //FIXME then insert this to addNode in BNScene
-            // Get the ID of the NSManagedObject
-            /*
-            if let id : NSManagedObjectID? = persistentStoreCoordinator?.managedObjectIDForURIRepresentation(object as NSURL){
-                
 
-            }
-            */
+        for object : AnyObject in draggedArray{
+            self.addNode(object as NSURL)
+           
         }
-        /*
-        if(types.containsObject(kUTTypeURL)){
-            println("kutt found")
-            //let ustr : NSString = pboard.stringForType(NSString (format: kUTTypeURL))!
-           // let ustr : NSString = pboard.stringForType((NSString *)kUTTypeURL)
-            //let mo : NSURL = NSURL(string: ustr)
-           // println(mo)
-        }
-        */
+
         return false
     }
     
     func addNode(mourl: NSURL){
         var errorPtr : NSErrorPointer = nil
-       // println("addnode \(mourl)")
         if let id : NSManagedObjectID? = moc.persistentStoreCoordinator?.managedObjectIDForURIRepresentation(mourl){
             
             var mo : NodeLink = moc.objectWithID(id!) as NodeLink
             
             
             var newNode : BNNode = BNNode(entity: NSEntityDescription.entityForName("BNNode", inManagedObjectContext: moc)!, insertIntoManagedObjectContext: moc)
-            //newNode.setValue(newNode.name, forKey: "name")
-            newNode.setValue("added", forKey: "name")
+            newNode.setValue(mo.name, forKey: "name")
+            //newNode.setValue("added", forKey: "name")
             newNode.setValue(mo, forKey: "nodeLink")
             
-    //        yes but what about recirocal add for the node
+
+        
 
             var curModels : [Model] = modelTreeController.selectedObjects as [Model]
             var curModel : Model = curModels[0]
-            
-            
             curModel.addBNNodeObject(newNode)
+
             newNode.setValue(curModel, forKey: "model")
             
             
             
             moc.save(errorPtr)
+
             
         }
         
