0b21a234e398673e0622b592b2100cd426bdd454
matt <matt@Hearth.local>
2015-05-04 14:01:37 -0700

    added deviate functions

--- Plexus/Bridging-Header.h
+++ Plexus/Bridging-Header.h
@@ -9,10 +9,25 @@
 #ifndef Plexus_Bridging_Header_h
 #define Plexus_Bridging_Header_h
 
+
 #import <CorePlot/CorePlot.h>
 #import "CPTMutablePlotRange+SwiftCompat.h"
 //#import "CPTXYAxis+SwiftCompat.h"
 #import "PlexusCalculationOperation.h"
-//#import "bnkernel.cl.h"
-//#import "bn_kernel.cl"
+
+
+
+// A routine for generating good random numbers
+extern double ran3(long *idum);
+
+//returns gaussian deviates
+double gasdev(long *idum);
+
+//returns gamma deviates
+extern double gamma_dev(double a);
+extern double igamma_dev(int ia);
+
+double beta_dev(double a, double b);
+
+
 #endif
