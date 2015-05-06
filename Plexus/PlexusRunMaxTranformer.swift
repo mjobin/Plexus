d9133de00f1ed8d0a263c3753cfcf8a33cb89805
matt <matt@Hearth.local>
2015-05-06 08:03:12 -0700

    initial import

---
+++ Plexus/PlexusRunMaxTranformer.swift
@@ -0,0 +1,44 @@
+//
+//  PlexusRunMaxTranformer.swift
+//  Plexus
+//
+//  Created by matt on 5/6/2015.
+//  Copyright (c) 2015 Santa Clara University. All rights reserved.
+//
+
+import Cocoa
+
+class PlexusRunMaxTransformer: NSValueTransformer {
+    
+    
+    override class func transformedValueClass() -> AnyClass {
+        return NSNumber.self
+    }
+    
+    override func  transformedValue(value: AnyObject?) -> AnyObject? {
+        
+        
+        if(value == nil){
+            return NSNumber(int: 1)
+        }
+        
+        
+        
+        var outVal : NSNumber = value as! NSNumber
+        
+        if(outVal.integerValue < 1) {
+            return 1
+        }
+            
+            
+        else {
+            return (outVal.integerValue-1)
+        }
+        
+        
+        
+        
+    }
+    
+    
+}
---
+++ Plexus/PlexusRunMinTransformer.swift
@@ -0,0 +1,44 @@
+//
+//  PlexusRunMinTransformer.swift
+//  Plexus
+//
+//  Created by matt on 5/4/2015.
+//  Copyright (c) 2015 Santa Clara University. All rights reserved.
+//
+
+import Cocoa
+
+class PlexusRunMinTransformer: NSValueTransformer {
+    
+    
+    override class func transformedValueClass() -> AnyClass {
+        return NSNumber.self
+    }
+    
+    override func  transformedValue(value: AnyObject?) -> AnyObject? {
+        
+        
+        if(value == nil){
+        return NSNumber(int: 1)
+        }
+        
+        
+        
+        var outVal : NSNumber = value as! NSNumber
+        
+        if(outVal.integerValue < 1) {
+            return 1
+        }
+        
+        
+        else {
+        return (outVal.integerValue+1)
+        }
+        
+        
+        
+
+    }
+
+
+}
