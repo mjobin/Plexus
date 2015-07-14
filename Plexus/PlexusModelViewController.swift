0db537edf5fcfb012a062d93975ff5389c6544cd
redredeyes <matt@Hearth.local>
2015-07-14 16:18:12 -0400

    added childModel fxn

--- Plexus/PlexusModelViewController.swift
+++ Plexus/PlexusModelViewController.swift
@@ -29,4 +29,77 @@ class PlexusModelViewController: NSViewController {
         // Do view setup here.
     }
     
+   @IBAction func childModel(sender : AnyObject){
+        var errorPtr : NSErrorPointer = nil
+    
+        let curDatasets : [Dataset] = datasetController.selectedObjects as! [Dataset]
+        let curDataset : Dataset = curDatasets[0]
+    
+        let curModels : [Model] = modelTreeController.selectedObjects as! [Model]
+        let curModel : Model = curModels[0]
+    
+    if let curPath : NSIndexPath = modelTreeController.selectionIndexPath {
+        let newPath : NSIndexPath = curPath.indexPathByAddingIndex(curModel.children.count)
+        
+
+
+        var newModel : Model = Model(entity: NSEntityDescription.entityForName("Model", inManagedObjectContext: self.moc)!, insertIntoManagedObjectContext: self.moc)
+        curModel.addChildObject(newModel)
+        newModel.setValue(curModel, forKey: "parent")
+        modelTreeController.insertObject(newModel, atArrangedObjectIndexPath: newPath)
+        let copyName : String = curModel.name + " copy"
+        newModel.setValue(copyName, forKey: "name")
+
+        
+        let curNodes  = curModel.bnnode.allObjects as! [BNNode]
+        for curNode : BNNode in curNodes {
+            var newNode : BNNode = BNNode(entity: NSEntityDescription.entityForName("BNNode", inManagedObjectContext: self.moc)!, insertIntoManagedObjectContext: self.moc)
+
+
+            newNode.setValue(curNode.nodeLink, forKey: "nodeLink")
+            
+
+            //curNode.nodeLink.addBNNodeObject(newNode)
+            newNode.setValue(curNode.cptFreq, forKey: "cptFreq")
+            newNode.setValue(curNode.dataName, forKey: "dataName")
+            newNode.setValue(curNode.dataScope, forKey: "dataScope")
+            newNode.setValue(curNode.dataSubName, forKey: "dataSubName")
+            newNode.setValue(curNode.numericData, forKey: "numericData")
+            newNode.setValue(curNode.priorDistType, forKey: "priorDistType")
+            newNode.setValue(curNode.priorV1, forKey: "priorV1")
+            newNode.setValue(curNode.priorV2, forKey: "priorV2")
+            
+            //Move postArray and post Count to new nodes' priorArray and priorCount, so that new sims can be run on previous results
+            newNode.setValue(curNode.postArray, forKey: "priorArray")
+            newNode.setValue(curNode.postCount, forKey: "priorCount")
+            
+            
+            let blankArray = [NSNumber]()
+            let blankData = NSKeyedArchiver.archivedDataWithRootObject(blankArray)
+            newNode.setValue(blankData, forKey: "postCount")
+            newNode.setValue(blankData, forKey: "postArray")
+            
+            
+            
+            
+            newNode.setValue(newModel, forKey: "model")
+            newModel.addBNNodeObject(newNode)
+            
+        }
+        
+
+        
+        newModel.setValue(curDataset, forKey: "dataset")
+        curDataset.addModelObject(newModel)
+        
+        
+        
+    }
+
+    
+        self.moc.save(errorPtr)
+
+    
+    }
+    
 }
