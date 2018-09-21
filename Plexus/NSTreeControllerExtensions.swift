38b981fffe3c59523fbae37e441415b26f345692
mjobin <oHK-u9f-Tef-u4H>
2018-09-21 07:53:23 -0700

    Me.

--- Plexus/NSTreeControllerExtensions.swift
+++ Plexus/NSTreeControllerExtensions.swift
@@ -3,7 +3,7 @@
 //  Plexus
 //
 //  Created by matt on 9/19/18.
-//  Copyright © 2018 Santa Clara University. All rights reserved.
+//  Copyright © 2018 Matthew Jobin. All rights reserved.
 //
 
 import Foundation
--- PlexusTests/PlexusTests.swift
+++ PlexusTests/PlexusTests.swift
@@ -3,7 +3,7 @@
 //  PlexusTests
 //
 //  Created by matt on 10/1/14.
-//  Copyright (c) 2014 Santa Clara University. All rights reserved.
+//  Copyright (c) 2014 Matthew Jobin. All rights reserved.
 //
 
 import Cocoa
